2608c8f3991242d9b50c0ff423273d8124b0dc30
Ankita Dhakar <dhakarankita71@gmail.com>
2025-06-17 08:46:11 +0530

    Add files via upload

---
+++ solution/pages/FoodPilot.jsx
@@ -0,0 +1,223 @@
+import React from "react";
+import "./Food.css";
+
+
+function FoodPilot(){
+ 
+    return(<>
+
+
+<div class="container">
+    <div class="left">
+      <h1><span class="bold-dark">Food</span><br/><span class="bold-green">Pilot</span></h1>
+    </div>
+    <div class="right">
+      <h2>Transform Your Food Production Processes</h2>
+      <p> 
+       At AgriPilot.ai, we offer Food Pilot, an innovative AI-driven solution designed to assist farmers & agronomists in enhancing various aspects of farming. It integrates advanced AI/ML and IoT technologies to optimize every stage of food production, quality, and distribution.
+      </p>  
+    </div>
+  </div>
+{/* --------------------------------------------------------------------------- */}
+
+ <section class="info-section">
+    <div class="text-content">
+      <h2>Elevating Farming,<br/>Maximizing Efficiency</h2>
+      <p>With Food Pilot, you can:</p>
+      <ul>
+        <li>Optimize irrigation, fertilization, and pest control</li>
+        <li>Plan and select crops for peak productivity</li>
+        <li>Minimize downtime and maintenance costs</li>
+        <li>Reduce waste and improve market efficiency</li>
+    
+      </ul>
+    </div>
+    <div class="image-content">
+      <img src=" /src/assets/components/solution/pages/food-1.jpeg" alt="Plant with nutrients"/>
+    </div>
+  </section>
+
+{/* -------------------------------------------------------------------- */}
+  <section class="capability-heading">
+    <h2>
+      Food Pilot Offers a Range of <br/>
+      Capabilities to Enhance Farming <br/>
+      Practices
+    </h2> 
+  </section>
+
+{/* --------------------------------------------- */}
+ <section class="precision-section">
+    <div class="precision-container">
+      <div class="precision-image">
+        <img src="/src/assets/components/solution/pages/food-dron.svg" alt="Drone spraying crops"/>
+      </div>
+      <div class="precision-text">
+        <img src="/src/assets/components/solution/pages/f1.png" alt="icon" class="icon"/>
+        <h2>Precision Agriculture</h2>
+        <p>
+          Food Pilot provides detailed insights into crop health, soil conditions, and weather patterns to optimize farming practices. 
+          Farmers can make informed decisions on irrigation, fertilization, and pest control, leading to better crop yields and efficient resource use.
+        </p>
+      </div>
+    </div>
+  </section>
+
+{/* ------------------- */}
+ <section class="precision-section">
+    <div class="precision-container">
+          <div class="precision-text">
+        <img src="/src/assets/components/solution/pages/f2.png" alt="icon" class="icon"/>
+        <h2>Smart Food Production</h2>
+        <p>
+         Assists in crop planning, selection, and harvesting, enabling farmers to optimize their crop cycles, choose the best crops for their specific conditions, and determine the ideal harvesting times. This leads to increased productivity and profitability.
+        </p>
+      </div>
+      <div class="precision-image">
+        <img src="/src/assets/components/solution/pages/food-2.svg" alt="Drone spraying crops"/>
+      </div>
+    </div>
+  </section>
+{/* ----------------------- */}
+ <section class="precision-section">
+    <div class="precision-container">
+      <div class="precision-image">
+        <img src="/src/assets/components/solution/pages/food-3.svg" alt="Drone spraying crops"/>
+      </div>
+      <div class="precision-text">
+        <img src="/src/assets/components/solution/pages/f3.png" alt="icon" class="icon"/>
+        <h2>Predictive Maintenance</h2>
+        <p>
+          Implements predictive maintenance for food processing equipment using AI. This feature helps minimize downtime, reduce maintenance costs, and ensure that all equipment operates at peak efficiency.
+        </p>
+      </div>
+    </div>
+  </section>
+{/* ------------------- */}
+ <section class="precision-section">
+    <div class="precision-container">
+          <div class="precision-text">
+        <img src="/src/assets/components/solution/pages/f4.svg" alt="icon" class="icon"/>
+        <h2>Sustainability Metrics</h2>
+        <p>
+         Food Pilot delivers recommendations for sustainable farming practices, promoting environmental health and long-term productivity. It enables farmers to make informed decisions on essential resources such as water and energy, ensuring long-term agricultural sustainability.
+        </p>
+      </div>
+      <div class="precision-image">
+        <img src="/src/assets/components/solution/pages/Sustainability_Metrics.svg" alt="Drone spraying crops"/>
+      </div>
+    </div>
+  </section>
+{/* ----------------------- */}
+ <section class="precision-section">
+    <div class="precision-container">
+      <div class="precision-image">
+        <img src="/src/assets/components/solution/pages/Silos_Management.svg" alt="Drone spraying crops"/>
+      </div>
+      <div class="precision-text">
+        <img src="/src/assets/components/solution/pages/f5.jpeg" alt="icon" class="icon"/>
+        <h2>Silos Management</h2>
+        <p>
+          Using IoT sensors, it allows farmers to measure grain temperature at various depths, capture real-time outdoor temperature and humidity, and automate aeration and cooling processes, providing a complete picture of each grain batch throughout storage.
+        </p>
+      </div>
+    </div>
+  </section>
+{/* ------------------- */}
+ <section class="precision-section">
+    <div class="precision-container">
+          <div class="precision-text">
+        <img src="/src/assets/components/solution/pages/f6.png" alt="icon" class="icon"/>
+        <h2>Quality Control</h2>
+        <p>
+         Ensures the highest quality of produce through continuous monitoring of freshness, nutritional value, and safety. This enables farmers to maintain high standards of produce quality, resulting in increased consumer trust and potentially higher market prices.
+        </p>
+      </div>
+      <div class="precision-image">
+        <img src="/src/assets/components/solution/pages/Quality_Control.svg" alt="Drone spraying crops"/>
+      </div>
+    </div>
+  </section>
+{/* ----------------------- */}
+ <section class="precision-section">
+    <div class="precision-container">
+      <div class="precision-image">
+        <img src="/src/assets/components/solution/pages/Market_Analysis.svg" alt="Drone spraying crops"/>
+      </div>
+      <div class="precision-text">
+        <img src="/src/assets/components/solution/pages/f7.png" alt="icon" class="icon"/>
+        <h2>Market Analysis</h2>
+        <p>
+          Offers data-driven insights on market trends and consumer preferences, helping farmers make informed decisions to align their production with market demand. This reduces the risk of overproduction or underproduction and improves profitability.
+        </p>
+      </div>
+    </div>
+  </section>
+{/* ------------------- */}
+ <section class="precision-section">
+    <div class="precision-container">
+          <div class="precision-text">
+        <img src="/src/assets/components/solution/pages/f8.png" alt="icon" class="icon"/>
+        <h2>Supply Chain Optimization</h2>
+        <p>
+         Streamlines logistics and inventory management to reduce waste and enhance efficiency, resulting in reduced post-harvest losses and improved efficiency in getting produce to market. This maximizes profitability.
+        </p>
+      </div>
+      <div class="precision-image">
+        <img src="/src/assets/components/solution/pages/Supply_Chain_Optimization.svg" alt="Drone spraying crops"/>
+      </div>
+    </div>
+  </section>
+
+
+
+{/* -------------------------------------------------------------------------- */}
+  <section class="benefits-section">
+    <div class="benefits-container">
+      <h2>Key Benefits</h2>
+      <div class="benefits-grid">
+        <div class="benefit-item">
+          <img src="/src/assets/components/solution/pages/food-key.webp" alt="Increased Efficiency Icon"/>
+          <h3>Increased Efficiency</h3>
+          <p>Automate and optimize production processes to save time and resources.</p>
+        </div>
+        <div class="benefit-item">
+          <img src="/src/assets/components/solution/pages/cllip-board.png" alt="Enhanced Quality Icon"/>
+          <h3>Enhanced Quality</h3>
+          <p>Maintain high standards and reduce waste with real-time monitoring.</p>
+        </div>
+        <div class="benefit-item">
+          <img src="/src/assets/components/solution/pages/key-3.png" alt="Sustainable Practices Icon"/>
+          <h3>Sustainable Practices</h3>
+          <p>Promote environmentally friendly practices through efficient resource management.</p>
+        </div>
+        <div class="benefit-item">
+          <img src="\src\assets\components\solution\pages\food-key-1.webp" alt="Improved Traceability Icon"/>
+          <h3>Improved Traceability</h3>
+          <p>Ensure transparency and accountability throughout the supply chain.</p>
+        </div>
+        <div class="benefit-item">
+          <img src="/src/assets/components/solution/pages/key-2.png" alt="Cost Savings Icon"/>
+          <h3>Cost Savings</h3>
+          <p>Reduce maintenance costs and downtime with predictive maintenance.</p>
+        </div>
+        <div class="benefit-item">
+          <img src="/src/assets/components/solution/pages/earth.png" alt="Market Responsiveness Icon"/>
+          <h3>Market Responsiveness</h3>
+          <p>Stay ahead of market trends with comprehensive consumer insights.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+
+
+
+
+    </>)
+}
+export default FoodPilot;
---
+++ solution/pages/SoilPilot.jsx
@@ -0,0 +1,247 @@
+import React from "react";
+import "./Soil.css";
+
+
+function SoilPilot(){
+ 
+
+
+
+
+    return(<>
+
+<div class="container">
+    <div class="left">
+      <h1><span class="bold-dark">Soil</span><br/><span class="bold-green">Pilot</span></h1>
+    </div>
+    <div class="right">
+      <h2>Enhancing Soil Health Management with NewTech</h2>
+      <p>
+        Farming sustains our world, relying on healthy soil for optimal productivity. However, traditional soil analysis methods often fall short, leading to failures, high costs, & limited access. 
+        <strong>At AgriPilot.ai, we have introduced Soil Pilot, the most accurate, portable, and affordable soil health monitoring device.</strong>
+      </p>
+      <p class="highlight">Say goodbye to costly lab tests and waiting for results!</p>
+    </div>
+  </div>
+
+
+{/* --------------------------------------------------------------------------- */}
+
+ <section class="info-section">
+    <div class="text-content">
+      <h2>Empowering Farmers,<br/>Enhancing Yields</h2>
+      <p>With Soil Pilot, you can:</p>
+      <ul>
+        <li>Measure over 14 essential parameters</li>
+        <li>Calculate water nutrients</li>
+        <li>Obtain AI-based advisory reports in minutes</li>
+        <li>Direct communication for data-driven decisions</li>
+        <li>Optimize soil health</li>
+      </ul>
+    </div>
+    <div class="image-content">
+      <img src=" /src/assets/components/solution/pages/plant.jpg" alt="Plant with nutrients"/>
+    </div>
+  </section>
+
+
+{/* --------------------------------------------------------------- */}
+
+<section class="testing-matters">
+    <h2>Why Soil Testing Matters?</h2>
+    <p class="subtitle">Without soil testing, you risk :</p>
+
+    <div class="risk-items">
+      <div class="risk-box">
+        <img src="/src/assets/components/solution/pages/soil-1.svg" alt="Reduced crop quality"/>
+        <p><strong>Reduced crop<br/>quality</strong></p>
+      </div>
+      <div class="risk-box">
+        <img src="/src/assets/components/solution/pages/soil-2.png" alt="Inefficient resource use"/>
+        <p><strong>Inefficient<br/>resource use</strong></p>
+      </div>
+      <div class="risk-box">
+        <img src="/src/assets/components/solution/pages/earth-2.png" alt="Negative environmental impacts"/>
+        <p><strong>Negative<br/>environmental impacts</strong></p>
+      </div>
+    </div>
+
+    <p class="footer-text">
+      Soil Pilot’s AI innovation ensures precise data for informed decisions & sustainable farming practices.
+    </p>
+  </section>
+
+
+
+{/* ------------------------------------------------------- */}
+
+<div className="soil-a">
+    <div className="soil-content">
+        <h3>Soil Pilot’s Advanced  , <br />Solutions for Soil Health </h3>
+     
+      <ul>
+        <li> <img src="/src/assets/components/solution/pages/Aerospace-Icon-1.png" alt="" /> Conducts soil grid analysis to collect AI-based data from satellite imagery</li>
+     <li> <img src="/src/assets/components/solution/pages/hot-thermometer-2.webp" alt="" /> Utilizes IoT sensors and Soil Pilot's Soil Testing Kit for real-time data collection</li>
+    <li><img src="/src/assets/components/solution/pages/folder-icon.webp" alt="" /> AI algorithms determine optimal soil sensor placement for accurate reports</li>  
+    <li><img src="/src/assets/components/solution/pages/farm-tractor.webp" alt="" /> AI algorithms to identify soil sample locations and AI-assisted Rovers to collect samples</li>  
+    <li><img src="/src/assets/components/solution/pages/Weather_proof.png" alt="" /> Provides real-time weather forecasts sourced from Azure Weather</li>
+    <li><img src="/src/assets/components/solution/pages/tower.png" alt="" /> Integrated weather station APIs aid in strategic farm planning</li> 
+      </ul>
+
+    </div>
+
+
+</div>
+
+{/* -------------------------------------------------- */}
+  <div class="heading-container">
+    <h1>Soil Parameter Details Key Options</h1>
+  </div>
+
+
+
+{/* ----------------------------------------------------- */}
+
+  <div class="container1">
+
+    <div class="cards">
+      <div class="card">
+        <img src="/src/assets/components/solution/pages/tech-1.svg" alt="No-Touch"/>
+        <div class="card-text">
+          <h2 class="title">No-Touch</h2>
+          <p><strong>(AI-based Data)</strong></p>
+          <p>Utilizes satellite imagery to estimate soil parameters and categorize them into low, medium, and high ranges with 30–70% accuracy, delivering results in just 1 minute.</p>
+        </div>
+      </div>
+
+      <div class="card">
+        <img src="/src/assets/components/solution/pages/tech-2.svg" alt="Low-Touch"/>
+        <div class="card-text">
+          <h2 class="title">Low-Touch</h2>
+          <p><strong>(Sensor-based Data)</strong></p>
+          <p>Leveraging the LoRaWAN low-power wide-area networking protocol, manage communication between end-node IoT devices. This allows it to collect real-time soil data through IoT sensors installed on the field  on the farm, offering 80-90% accuracy within 20 minutes. This option is available only if the user has requested sensor installation.</p>
+        </div>
+      </div>
+
+      <div class="card">
+        <img src="/src/assets/components/solution/pages/tech-3.svg" alt="High-Touch"/>
+        <div class="card-text">
+          <h2 class="title">High-Touch</h2>
+          <p><strong>(Lab Report)</strong></p>
+        <p>Integrates rover-based soil sample collection with AI workflows to produce accurate nutrient heatmaps, aiding in spot fertilization and irrigation management. Detailed soil parameter data is displayed on SoilPilot.ai Soil Testing Kit, with 80-90% accuracy within 45 minutes.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+{/* ======================================================================= */}
+
+  <div class="process-container">
+    <h1>How Soil Pilot Works</h1>
+    <p class="subtitle">Precision in every step</p>
+    <div class="process-steps">
+      <div class="step">
+        <img src="/src/assets/components/solution/pages/tube-1.svg" alt="Collect Sample"/>
+        <p><strong>Collect Soil Sample<br/>& Add to Solution</strong></p>
+      </div>
+      <div class="step">
+        <img src="/src/assets/components/solution/pages/tube-2.svg" alt="Shake and Filter"/>
+        <p><strong>Shake the<br/>Solution & Filter</strong></p>
+      </div>
+      <div class="step">
+        <img src="/src/assets/components/solution/pages/tube-3.svg" alt="Test and Report"/>
+        <p><strong>Perform Test &<br/>Provide Report</strong></p>
+      </div>
+    </div>
+  </div>
+
+
+{/* --------------------------------------------------------- */}
+
+ <section class="features-section">
+    <h1>Key Features of Soil Pilot</h1>
+    <p>Implement the power of technologies with AgriPilot.ai</p>
+  </section>
+
+{/* ----------------------------------------------------------- */}
+ <section class="feature-block">
+    <div class="feature-image">
+      <img src="/src/assets/components/solution/pages/solar-pannel.svg" alt="Solar Panel"/>
+    </div>
+    <div class="feature-details">
+      <div class="feature-item">
+        <h3>Solar-Powered & Portable</h3>
+        <p>Quick setup and use</p>
+      </div>
+      <hr />
+      <div class="feature-item">
+        <h3>Swift Reporting</h3>
+        <p>Lab-quality printed reports within 35–45 minutes</p>
+      </div>
+      <hr/>
+      <div class="feature-item">
+        <h3>Comprehensive Analysis</h3>
+        <p>Measure 14+ soil and water nutrients</p>
+      </div>
+      <hr/>
+      <div class="feature-item">
+        <h3>Connectivity Options</h3>
+        <p>Bluetooth and USB for seamless access</p>
+      </div>
+      <hr/>
+      <div class="feature-item">
+        <h3>AGI-Based Recommendations</h3>
+        <p>Optimize crop selection</p>
+      </div>
+    </div>
+  </section>
+
+
+{/* ------------------------------------------------------------- */}
+ <section class="benefits-section">
+    <h2>Key Benefits</h2>
+    <div class="benefits-container">
+      <div class="benefit-item">
+        <img src="/src/assets/components/solution/pages/cllip-board.png" alt="Informed Decisions"/>
+        <h3>Informed Decisions</h3>
+        <p>AGI-based recommendations on crops, cover crops, sustainability metrics, fertilizers, and water usage helps to make data-driven decisions.</p>
+      </div>
+      <div class="benefit-item1">
+        <img src="/src/assets/components/solution/pages/key-2.png" alt="Cost Reduction"/>
+        <h3>Cost Reduction</h3>
+        <p>Achieve a 20–25% reduction in costs through optimized resource management.</p>
+      </div>
+      <div class="benefit-item">
+        <img src="/src/assets/components/solution/pages/key-3.png" alt="Increased Yield"/>
+        <h3>Increased Yield</h3>
+        <p>Boosts crop yield by 20–30% with precise and timely recommendations.</p>
+      </div>
+      <div class="benefit-item">
+        <img src="/src/assets/components/solution/pages/earth.png" alt="Enhanced Sustainability"/>
+        <h3>Enhanced Sustainability</h3>
+        <p>Promotes sustainable farming practices through advanced metrics analysis.</p>
+      </div>
+    </div>
+  </section>
+
+
+{/* ----------------------------------------------------- */}
+ <section class="features-section1">
+    <h1>Embark On the Journey Towards Modern Agriculture with Soil Pilot…!</h1>
+  </section>
+
+
+
+
+
+
+
+
+
+
+
+    </>)
+}
+export default SoilPilot;
